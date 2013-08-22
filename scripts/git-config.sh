3e62802be1c4db2d8ebc3cb0653361fc725a5299
Rocky Zheng <peiqzhen@cisco.com>
2013-08-22 03:51:00 -0700

    add some useful scripts

---
+++ scripts/git-config.sh
@@ -0,0 +1,8 @@
+#/bin/sh
+
+git config --global user.name 'Rocky Zheng'
+git config --global user.email peiqzhen@cisco.com
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.st status
